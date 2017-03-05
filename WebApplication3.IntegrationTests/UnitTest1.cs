c30a4fa2d7644fb517e86fe59511e1c9d7364bae
Viktor <viktortsvetkov@yahoo.com>
2017-03-05 20:39:52 +0200

    no message

---
+++ WebApplication3.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebApplication3.IntegrationTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
